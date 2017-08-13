2f019ac4c75dfbbf03affe0e585a2eff28f34740
Aagam Shah <aagams68@gmail.com>
2017-08-13 21:03:47 +0530

    Refactor Login UI and integrate Firebase auth

--- app/src/main/java/com/systers/conference/ConferenceApplication.java
+++ app/src/main/java/com/systers/conference/ConferenceApplication.java
@@ -4,6 +4,9 @@ import android.app.Application;
 import android.content.Context;
 
 import com.facebook.stetho.Stetho;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
 import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -29,5 +32,9 @@ public class ConferenceApplication extends Application {
                         .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
                         .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
                         .build());
+        TwitterConfig twitterConfig = new TwitterConfig.Builder(this)
+                .debug(true)
+                .twitterAuthConfig(new TwitterAuthConfig(BuildConfig.TwitterApiKey, BuildConfig.TwitterSecretKey)).build();
+        Twitter.initialize(twitterConfig);
     }
 }
